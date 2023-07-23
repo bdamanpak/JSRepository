8b581dc34640683a5d357bf553a4b39f0969bd59
Bardia Damanpak <damanpak_bardia@yahoo.com>
2023-07-23 12:16:12 +0330

    1st commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Get the input
+let first = +prompt("enter the first number :");
+let second = +prompt("enter the second number :");
+let sum = first + second;
+
+// Show the result
+console.log(sum);
+document.write(sum);
+alert(sum);
